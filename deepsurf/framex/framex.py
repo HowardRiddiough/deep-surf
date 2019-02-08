ad1dde6d25165ad60febebffd5f9df0c65dc045d
How <riddiough_h@schiphol.nl>
2019-02-08 17:13:01 +0100

    wip

--- deepsurf/framex/framex.py
+++ deepsurf/framex/framex.py
@@ -1,7 +1,9 @@
+from collections import OrderedDict
 import os
 import urllib
 from datetime import datetime, timedelta, timezone
 from time import sleep
+import logging
 
 import numpy as np
 import cv2
@@ -9,35 +11,52 @@ from pytesseract import image_to_string
 from skyfield import api
 from skyfield import almanac
 
-X = 0
-Y = 0
-H = 30
-W = 1280
+# logger = logging.getLogger(__file__)
+# logger.setLevel(logging.INFO)
+
+CAMS = dict(
+    surf="http://www.scheveningenlive.nl/cam_1.jpg",
+    sports="http://www.scheveningenlive.nl/sport.jpg"
+)
+
+CAM_CROP = dict(
+    sports=OrderedDict(
+        x_min=30, 
+        x_max=550,
+        y_min=0, 
+        y_max=20
+    ),
+    surf=OrderedDict(
+        x_min=40,
+        x_max=850,
+        y_min=0,
+        y_max=25
+    )
+)
 
 SCH_LATITUDE_DEGS = 52.10550355970487
 SCH_LONGITUDE_DEGS = 4.265012741088867
 
-SURF_CAM_URL = "http://www.scheveningenlive.nl/cam_1.jpg"
-
 
 class SurfFrames:
-    def __init__(self, interval: int, out_path: str, cam_url: str = SURF_CAM_URL):
+    def __init__(self, interval: int, out_path: str):
         self.interval = interval
         self.out_path = out_path
-        self.cam_url = cam_url
 
     def get_frames(self):
+#         logging.info("Downloading frames for {} cameras every {} seconds".format(len(CAMS), self.interval))
         while True:
             if SurfFrames.is_dt_in_sunlight(datetime.now(timezone.utc)):
-                self._persist_frame_to_disk(self.cam_url, self.out_path)
+                for cam_name, cam_url in CAMS.items():
+                    self._persist_frame_to_disk(cam_url, self.out_path, cam_name)
                 sleep(self.interval)
             else:
                 sleep(self.interval)
 
     @staticmethod
-    def _persist_frame_to_disk(url: str, out_path: str):
+    def _persist_frame_to_disk(url: str, out_path: str, cam_name: str):
         frame_array = SurfFrames._request_frame_as_array(url)
-        cam_id, dt = SurfFrames._cam_id_timestamp_from_frame_text(frame_array)
+        cam_id, dt = SurfFrames._cam_id_timestamp_from_frame_text(frame_array, cam_name)
         cv2.imwrite(os.path.join(out_path, "{}_{}.jpg".format(cam_id, dt)), frame_array)
 
     @staticmethod
@@ -47,8 +66,9 @@ class SurfFrames:
         return cv2.imdecode(frame, cv2.IMREAD_COLOR)
 
     @staticmethod
-    def _cam_id_timestamp_from_frame_text(frame: np.ndarray):
-        crop_frame = frame[Y: Y + H, X: X + W]
+    def _cam_id_timestamp_from_frame_text(frame: np.ndarray, cam_name: str):
+        x_min, x_max, y_min, y_max = CAM_CROP[cam_name].values()
+        crop_frame = frame[y_min: y_max, x_min: x_max]
         cam_id, _, dt = image_to_string(crop_frame, lang="eng").lower().split("|")
         dt = datetime.strptime(dt, " %d-%m-%Y %H:%M:%S").strftime("%Y%m%d%H%M%S")
         return cam_id.replace(" ", ""), dt
